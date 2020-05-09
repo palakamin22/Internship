d803e21ec93ca20f2f9e09d57183f1e758749712
Palak <palakamin2206@gmail.com>
2020-05-09 19:30:25 +0530

    fragment update

--- BhavaniElectronics/app/src/main/java/com/example/bhavanielectronics/ui/home/HomeFragment.java
+++ BhavaniElectronics/app/src/main/java/com/example/bhavanielectronics/ui/home/HomeFragment.java
@@ -1,35 +1,25 @@
 package com.example.bhavanielectronics.ui.home;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.bhavanielectronics.R;
 
 public class HomeFragment extends Fragment {
-
-    private HomeViewModel homeViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        TextView homeDesc = root.findViewById(R.id.textView1);
+        String home = "We “Bhavani Engineering” have gained success in the market by manufacturing, exporting and supplying a durable collection of Screw Barrel (single,Twin parallel, conical, injection moulding) food process screw barrel, various types of blades.We are exporting our products around the world.  We are a Sole Proprietorship company that is incorporated in the year 2008 at Ahmedabad (Gujarat, India) and always strive hard to provide our valued customers with qualitative range of barrels and sleeves as per the global set standards.We are a well known firm that is managed under the supervision of our Proprietor  “Mr. Kamlesh Patel” and have gained enormous clientele across the nation.";
+        homeDesc.setMovementMethod(new ScrollingMovementMethod());
+        homeDesc.setText(home);
         return root;
     }
 }
